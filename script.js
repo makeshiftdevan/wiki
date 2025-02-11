746d433264dae58a2f8d1f2f7831de84ef2a261e
makeshiftdevan <64051402+makeshiftdevan@users.noreply.github.com>
2025-02-10 21:45:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+<div id="arrow" class="scroll-down"></div>
+  
+  <div id="links">
+    <a href="/cdn-cgi/l/email-protection#0c6f6362786d6f784c7b65676578636722626978">contact</a><span>&nbsp;|&nbsp;</span>
+    <a target="_BLANK" href="https://movietok.app">movietok</a><span>&nbsp;|&nbsp;</span>
+    <a target="_BLANK" href="https://quotetok.com">quotetok</a>
+  </div>
+  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+  <script>
+    if (localStorage.getItem("scroll")) {
+      document.getElementById("arrow").remove();
+    }
+    setTimeout(function(){
+      if (document.getElementById("arrow")){
+        document.getElementById("arrow").remove();
+        localStorage.setItem("scroll","1");
+      }
+    },10000);
+  </script>
+  <script defer data-domain="wikitok.net" src="https://milkymouse.com/js/script.js"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2XBW30R7BY"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-2XBW30R7BY');
+  </script>
